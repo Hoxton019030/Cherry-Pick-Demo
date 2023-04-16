e92a58fc731d807b608617af153d6d4add79bb25
Hoxton <za546321@yahoo.com.tw>
2023-04-16 17:29:36 +0800

    modify: write login method

--- src/main/java/com/example/cherrypickdemo/controller/LoginController.java
+++ src/main/java/com/example/cherrypickdemo/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.example.cherrypickdemo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,4 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
+    @PostMapping("/login")
+    public String login(){
+        return "login succeed";
+    }
 }
