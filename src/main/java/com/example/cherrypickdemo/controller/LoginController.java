7eb2aa0fb296566645e8a674b10298cabd3146f7
Hoxton <za546321@yahoo.com.tw>
2023-04-16 17:27:57 +0800

    add : add login controller

---
+++ src/main/java/com/example/cherrypickdemo/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.cherrypickdemo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Hoxton
+ * @version 0.1.0
+ * @since 0.1.0
+ **/
+@RestController
+public class LoginController {
+
+}
